0c10f9c9fbf6ce21c0a100f6d045bbbaa0f7dde5
Felipe Salazar <fesago90@gmail.com>
2018-02-16 21:46:48 -0800

    ls alias

--- sh/.sharedrc/sharedrc.sh
+++ sh/.sharedrc/sharedrc.sh
@@ -1,6 +1,9 @@
 # init LS_COLORS from .dircolors
 eval $(dircolors -b $HOME/.sharedrc/dircolors)
 
+# aliases
+alias ls="ls -ah --color --group-directories-first"
+
 # config fzf to use ripgrep
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
