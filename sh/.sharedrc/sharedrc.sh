1c45a61a66103c058a90a58f75289dd7c1a02b35
Felipe Salazar <felipesalazar@google.com>
2018-04-16 23:43:37 -0700

    add PS1 for color prompt

--- sh/.sharedrc/sharedrc.sh
+++ sh/.sharedrc/sharedrc.sh
@@ -4,6 +4,12 @@ eval $(dircolors -b $HOME/.sharedrc/dircolors)
 # aliases
 alias ls="ls -ah --color --group-directories-first"
 
+function nonzero_return() {
+	RETVAL=$?
+	[ $RETVAL -ne 0 ] && echo -e " \033[48;5;1m$RETVAL\033[m"
+}
+export PS1="\A \[\033[38;5;11m\]\w\[\033[m\]\`nonzero_return\` \$ "
+
 # config fzf to use ripgrep
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
