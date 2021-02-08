26b0a6697e917f01191e9feb0bc59048a1eb33bc
Felipe Salazar <fesago90@gmail.com>
2021-02-07 21:57:29 -0800

    Add i3status conf

--- i3/install.sh
+++ i3/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source ../sh/common.sh
 
-link_file "${PWD}/config" "${HOME}/.config/i3/config" || LOGWARNING "Can't link i3 config  "
+link_file "${PWD}/config" "${HOME}/.config/i3/config" || LOGWARNING "Can't link i3 config"
+link_file "${PWD}/i3status.conf" "${HOME}/.config/i3status/config" || LOGWARNING "Can't link i3 status config"
