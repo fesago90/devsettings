56638fbc6e4697c1e8d900fea0940018721cb7e5
Felipe Salazar <fesago90@gmail.com>
2024-03-02 13:29:34 -0500

    fix cgdb locationg

--- cgdb/install.sh
+++ cgdb/install.sh
@@ -1,4 +1,4 @@
 source ../sh/common.sh
 
-mkdir -p "${HOME}/cgdb"
-link_file "${PWD}/cgdbrc" "${HOME}/.cgdb" || LOGWARNING "Can't link cgdbrc"
+mkdir -p "${HOME}/.cgdb"
+link_file "${PWD}/cgdbrc" "${HOME}/.cgdb/cgdbrc" || LOGWARNING "Can't link cgdbrc"
